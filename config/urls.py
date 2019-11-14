f1881d69ca79fef93c7bc6f861271ea9ebb9385d
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2019-11-14 09:36:02 -0600

    more formatting & content

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from gatherings.views import IndexView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("", IndexView.as_view(), name="home"),
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),

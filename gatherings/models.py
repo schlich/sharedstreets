bdee5da183404be218657065e4a06e08f51f2c06
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2019-11-24 12:26:15 -0600

    format gatherings template

--- gatherings/models.py
+++ gatherings/models.py
@@ -50,11 +50,12 @@ class Person(models.Model):
 class Gathering(models.Model):
     airtableID = models.CharField(max_length=20, blank=True, null=True)
     name = models.CharField(max_length=200)
-    description = models.TextField(max_length=300)
+    description = models.TextField()
+    location = models.CharField(max_length=50, null=True)
     category = models.CharField(max_length=2, choices=GATHERING_TYPES)
     authors = models.ManyToManyField(Person)
     dates = RecurrenceField(null=True, blank=True)
-
+    dates_description = models.TextField(null=True)
 
     def __str__(self):
         return self.name

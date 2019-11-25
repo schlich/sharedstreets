e5e8996fcdf0893645600329df944bf19efd2019
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2019-11-24 22:12:50 -0600

    allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,8 +6,9 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["sharedstreetsstl.org"])
-
+ALLOWED_HOSTS_1 = env.list("DJANGO_ALLOWED_HOSTS", default=["sharedstreetsstl.org"])
+ALLOWED_HOSTS_2 = ["sharedstreetsstl.org"]
+ALLOWED_HOSTS = ALLOWED_HOSTS_1 + ALLOWED_HOSTS_2
 # DATABASES
 # ------------------------------------------------------------------------------
 DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405

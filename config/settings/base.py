6035744c5afba64a25c89644c589d7e68a245c76
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2020-01-30 00:52:13 -0600

    add django-scheduler

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,6 +81,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "shared_streets.users.apps.UsersConfig",
     "gatherings",
+    "schedule"
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps

8effe3e2a076b80fcb28f0a03f6e7950f15b4159
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2020-02-25 03:02:39 -0600

    remove django-schedule

--- gatherings/models.py
+++ gatherings/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from recurrence.fields import RecurrenceField
-from schedule.models import Event
 from datetime import datetime
 
 
